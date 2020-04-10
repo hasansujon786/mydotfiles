e0f1d44f69c4c33586c40544b7bcc9a70d8ac466
Hasan Mahmud <hasansujon786@gmail.com>
2020-04-11 00:17:44 +0600

    fix typo

--- install.sh
+++ install.sh
@@ -103,4 +103,4 @@ prompt_and_get_answers() {
 prompt_and_get_answers
 # git config --global user.email "hasansujon786@gmail.com"
 # git config --global user.name "Hasan Mahmud"
-# git config --global credential.helper cache -timeout=86400
+# git config --global credential.helper 'cache --timeout=86400'
